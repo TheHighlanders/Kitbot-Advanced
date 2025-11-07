a84eb3e626eebb0cd68e2f2be09491e898547163
Adriana Massie <adrianammassie@gmail.com>
2025-11-07 02:34:28 -0500

    added stuff

---
+++ KitbotPID/src/main/java/frc/robot/subsystems/CoralReleaser.java
@@ -0,0 +1,62 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkMax;
+
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class CoralReleaser extends SubsystemBase {
+  /*
+   * Creates a Sparkmax Object so we can control the Sparkmax that is plugged into the motor
+   * The name of the object is dropper, the ID is 5 and the motor is brushed (it's a CIM motor)
+   */
+  private SparkMax Dropper = new SparkMax(5,MotorType.kBrushless);
+  /*
+   *   Hint: Objects and variables are declared here
+   */
+
+
+  public CoralReleaser() {
+    /*
+     * Hint: This is where the NEO's and Sparkmax's settings and/or configurations are set up.
+     * Anything that interacts with the NEOs and Sparkmax goes here too
+     */
+  
+    // For Elastic and Advtange Scope
+    SmartDashboard.putNumber("PID/kP", kP);
+    SmartDashboard.putNumber("PID/kI", kI);
+    SmartDashboard.putNumber("PID/kD", kD);
+    SmartDashboard.putNumber("Target RPM", targetRPM);
+  }
+
+  /*
+   * Hint: New Commands and Methods go here
+   */
+
+  @Override
+  // This method will be called once per scheduler run
+  public void periodic() {
+    // For Elastic and Advtange Scope
+    double newP = SmartDashboard.getNumber("PID/kP", kP);
+    double newI = SmartDashboard.getNumber("PID/kI", kI);
+    double newD = SmartDashboard.getNumber("PID/kD", kD);
+    double newTargetRPM = SmartDashboard.getNumber("Target RPM", targetRPM);
+   
+    SmartDashboard.putNumber("Dropper Setpoint", targetRPM);   // Setpoint
+    SmartDashboard.putNumber("Dropper Velocity", coralEncoder.getVelocity()); // Actual velocity
+ 
+  }
+}
---
+++ KitbotPID/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,75 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+
+public class Drivetrain extends SubsystemBase {
+  private final SparkMax leftSparkMax = new SparkMax(1, MotorType.kBrushed); //leftleader
+  private final SparkMax leftSparkMax2 = new SparkMax(2, MotorType.kBrushed); //leftfollower
+
+  private final SparkMax rightSparkMax = new SparkMax(3, MotorType.kBrushed); //rightleader
+  private final SparkMax rightSparkMax2 = new SparkMax(4, MotorType.kBrushed);//rightfollower
+
+  private Timer timer = new Timer();
+
+  private DifferentialDrive dih = new DifferentialDrive(leftSparkMax::set, rightSparkMax::set); 
+
+  public Drivetrain() {
+    // Creates the configuration (aka config) to apply to motors
+    SparkMaxConfig config = new SparkMaxConfig();
+
+    // Sets the config to follow the leftSparkMax then apply it to leftSparkMax2
+    // restmode is for if the sparkmax is swapped
+    // presist is for in case the sparkmax resets due to a breaker trip
+    config.follow(leftSparkMax);
+    leftSparkMax2.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    config.follow(rightSparkMax);
+    rightSparkMax2.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    // removes the following then applies the config to rightSparkMax
+    config.disableFollowerMode();
+    rightSparkMax.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    // sets the config to be inverted and applies it to the leftsparkmax
+    // this means positive values drive both sides forward
+    config.inverted(true);
+    leftSparkMax.configure(config, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+  } 
+  
+  public void go(double x, double y) {
+    dih.arcadeDrive(x, y);
+  }
+  /**
+   * Drives for x seconds then stops motor
+   */
+  public Command drive(double seconds, double fwSpeed, double zRot) {
+    timer.restart();
+    return runOnce (
+    () -> {
+      while (timer.get() < seconds) {
+        go(fwSpeed, zRot);
+      }
+      go(0, 0);
+    }); 
+  }
+
+  public Command driveautCommand(double x, double y){
+    return runOnce(
+    ()-> {
+      go(x, y);
+    }); 
+  }  
+}
